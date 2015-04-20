a1585253e45da919a4d3c70fead3e0b9df307fee
Edward Smith <edsmith6301@gmail.com>
2015-04-20 14:18:49 -0700

    adding server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(process.env.port || 5000, function() {
+  console.log('its running');
+});
+
+app.get('/*', function (req, res) {
+  console.dir(res);
+  res.sendStatus(404);
+});
+
+//testing
