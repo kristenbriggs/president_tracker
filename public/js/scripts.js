27150e55e5c0852edfa39bc895da62ebd6f645d6
Edward Smith <edsmith6301@gmail.com>
2015-04-22 12:45:17 -0700

    Added dialog boxes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -26,3 +26,105 @@ $(function() {
         $('#omalley').dialog('open');
     });
 });
+$(function() {
+    $('#cruz').dialog({
+        autoOpen:false,
+        width:500,
+        height:500,
+        modal: true,
+        buttons: {
+          Ok: function() {
+            $(this).dialog("close");
+        }
+    }});
+
+    $(".flex-item-2").click(function(e) {
+        e.preventDefault();
+        $('#cruz').dialog('open');
+    });
+});
+$(function() {
+    $('#webb').dialog({
+        autoOpen:false,
+        width:500,
+        height:500,
+        modal: true,
+        buttons: {
+          Ok: function() {
+            $(this).dialog("close");
+        }
+    }});
+
+    $(".flex-item-3").click(function(e) {
+        e.preventDefault();
+        $('#webb').dialog('open');
+    });
+});
+$(function() {
+    $('#webb').dialog({
+        autoOpen:false,
+        width:500,
+        height:500,
+        modal: true,
+        buttons: {
+          Ok: function() {
+            $(this).dialog("close");
+        }
+    }});
+
+    $(".flex-item-3").click(function(e) {
+        e.preventDefault();
+        $('#webb').dialog('open');
+    });
+});
+$(function() {
+    $('#rubio').dialog({
+        autoOpen:false,
+        width:500,
+        height:500,
+        modal: true,
+        buttons: {
+          Ok: function() {
+            $(this).dialog("close");
+        }
+    }});
+
+    $(".flex-item-4").click(function(e) {
+        e.preventDefault();
+        $('#rubio').dialog('open');
+    });
+});
+$(function() {
+    $('#clinton').dialog({
+        autoOpen:false,
+        width:500,
+        height:500,
+        modal: true,
+        buttons: {
+          Ok: function() {
+            $(this).dialog("close");
+        }
+    }});
+
+    $(".flex-item-5").click(function(e) {
+        e.preventDefault();
+        $('#clinton').dialog('open');
+    });
+});
+$(function() {
+    $('#paul').dialog({
+        autoOpen:false,
+        width:500,
+        height:500,
+        modal: true,
+        buttons: {
+          Ok: function() {
+            $(this).dialog("close");
+        }
+    }});
+
+    $(".flex-item-6").click(function(e) {
+        e.preventDefault();
+        $('#paul').dialog('open');
+    });
+});
